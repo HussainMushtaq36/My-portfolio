69764666b32f4b8b450c06b19560d7651834d7ae
Hussain Mushtaq <hussainmushtaq3636@gmail.com>
2026-07-21 13:54:15 +0500

    made the contact form working

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function sendMail()
 {
     let parms={
-         name=document.getElementById("name").value,
-         lastname=document.getElementById("lastname").value,
-         subject=document.getElementById("subject").value,
-        messages=document.getElementById("messages").value,
-         email=document.getElementById("email").value,
+         name:document.getElementById("name").value,
+         lastname:document.getElementById("lastname").value,
+         subject:document.getElementById("subject").value,
+         email:document.getElementById("email").value,
+         message:document.getElementById("messages").value,
          
     }
-    emailjs.send("service_f8dszeo","template_4l886h9",parms).then(alert("email sent!!"))
-}
+    emailjs.send("service_f8dszeo","template_4l886h9",parms).then(
+        alert("email sent!!")
+        
+    )
+
+     document.querySelector(".form").reset();
+};
